92ba7a65409a15eaef96f6a4de6c88c4c330bcf3
Zamir Johl <zjohl@pivotal.io>
2017-08-30 17:39:33 -0500

    Add data structures to hold json response

--- validation/validator.go
+++ validation/validator.go
@@ -1,15 +1,64 @@
 package validation
 
-import "io"
+import (
+	"encoding/json"
+	"io"
+)
 
-type Validator struct{}
+type Validator struct {
+	requester HTTPRequester
+	url       string
+}
 
 type HTTPRequester func(string) (io.ReadCloser, error)
 
 func NewValidator(url string, requester HTTPRequester) Validator {
-	return Validator{}
+	return Validator{
+		requester: requester,
+		url:       url,
+	}
 }
 
 func (v *Validator) InvalidCustomers() ([]byte, error) {
+	_, err := v.getFirstPage()
+	if err != nil {
+		return nil, err
+	}
+
 	return nil, nil
+}
+
+func (v *Validator) getFirstPage() (*apiResponse, error) {
+	reader, err := v.requester(v.url)
+	if err != nil {
+		return nil, err
+	}
+	var resp *apiResponse
+	err = json.NewDecoder(reader).Decode(resp)
+	return resp, err
+}
+
+type apiResponse struct {
+	Validations []Validation
+	Customers   []Customer
+	Pagination  Pagination
+}
+
+type Validation struct {
+	Required bool
+	Type     string // TODO: make enum
+	Length   Length
+}
+
+type Length struct {
+	Min int
+	Max int
+}
+
+type Customer map[string]interface{}
+
+type Pagination struct {
+	CurrentPage int
+	PerPage     int
+	Total       int
 }
